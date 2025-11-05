75049909d03a969cf105d94ef71f25dab7ddc2f4
Davide <herrdavide@live.de>
2025-11-05 09:59:00 +0100

    correcting typos

--- src/examples/refactoring/weather_process.py
+++ src/examples/refactoring/weather_process.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 
-
 TEMP_THRESHOLD_C = 25
 
 WEATHER_DATA_PATH = "./../../../data/weather_data.csv"
@@ -12,7 +11,7 @@ OFFSET_C_TO_F = 32
 FACTOR_C_TO_F = 1.8
 
 
-def celcius_to_fahrenheit(temperature):
+def celsius_to_fahrenheit(temperature):
     temps_converted = temperature.copy()
     mask = temperature > TEMP_THRESHOLD_C
     temps_converted[mask] = temps_converted[mask] * FACTOR_C_TO_F + OFFSET_C_TO_F
@@ -32,13 +31,9 @@ def compute_mean_wind_speed(obs):
     horizontal_wind_speed = (obs["wind_u"] ** 2 + obs["wind_v"] ** 2) **0.5
     return horizontal_wind_speed.mean()
 
- 
-
-
-
 def main():
     records = read_weather_data()
-    records["temp"] = celcius_to_fahrenheit(records["temp"])
+    records["temp"] = celsius_to_fahrenheit(records["temp"])
     mean_wind_speed = compute_mean_wind_speed(records)
     print("sum", records["temp"].sum())
     print("avg", records["temp"].mean())
