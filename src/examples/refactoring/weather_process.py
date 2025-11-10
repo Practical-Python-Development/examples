a3a06515e3a2c50758eb77cfdc9bdd34cbfc0003
grivera <gjriv616@gmail.com>
2025-11-10 15:17:35 +0100

    renamed funtions and methods. adjusted spacing and indentation

--- src/examples/refactoring/weather_process.py
+++ src/examples/refactoring/weather_process.py
@@ -1,5 +1,6 @@
 """This script is badly written on purpose to demonstrate refactoring."""
-import csv, math
+import csv
+import math
 
 
 OPEN_WEATHER_DATA = './../../../data/weather_data.csv'
@@ -8,35 +9,36 @@ FACTOR_C_TO_F = 1.8
 OFFSET_C_TO_F = 32
 
 
-def f(a):
-    t=[]
-    for i in a:
-        if float(i[1])> TEMP_THRESHOLD_C:
-            t.append(float(i[1]) * FACTOR_C_TO_F + OFFSET_C_TO_F)
+def convert_temperature(obs):
+    temps = []
+    for record in obs:
+        if float(record[1])> TEMP_THRESHOLD_C:
+            temps.append(float(record[1]) * FACTOR_C_TO_F + OFFSET_C_TO_F)
         else:
-            t.append(float(i[1]))
-    return t
+            temps.append(float(record[1]))
+    return temps
 
-def g(a):
-    s=0
-    for i in a:
-        s+=i
-    return s
+def sum_temperatures(temps):
+    sum_temps = 0
+    for i in temps:
+        sum_temps += i
+    return sum_temps
 
 
 r = open(OPEN_WEATHER_DATA)
-d=list(csv.reader(r))
+station_data = list(csv.reader(r))
 r.close()
-d=d[1:]
-x=[]
-for i in d:
-    x.append([i[0],i[1],i[2],i[3],i[4]])
-y=f(x)
-z=g(y)
-print('sum',z)
-print('avg',z/(len(y) if len(y) else 1))
-ws=0
-for i in d:
-    u=float(i[3]); v=float(i[4])
-    ws+=math.sqrt(u*u+v*v)
-print('wind',ws/len(d))
+station_data = station_data[1:]
+records = []
+for record in station_data:
+    records.append([record[0], record[1], record[2], record[3], record[4]])
+converted_temps = convert_temperature(records)
+total_temp = sum_temperatures(converted_temps)
+print('sum', total_temp)
+print('avg', total_temp / (len(converted_temps) if len(converted_temps) else 1))
+mean_wind_speed=0
+for record in records:
+    u=float(record[3])
+    v=float(record[4])
+    mean_wind_speed+=math.sqrt(u * u + v * v)
+print('wind', mean_wind_speed / len(records))
