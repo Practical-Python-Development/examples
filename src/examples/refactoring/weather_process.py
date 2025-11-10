0f856ee5c7612be75e4c9f77d69b63fbec810b1c
carlyotto13 <carlyotto13@gmail.com>
2025-11-10 15:01:57 +0100

    I refactored and introduced constants variables to the script. I introduced the new data set PATH_WEATHER_DATA, and temperature threshold, factor c to f, and offset c to f. I set these constants equal to values, and then incorporated the variable names into the script.

--- src/examples/refactoring/weather_process.py
+++ src/examples/refactoring/weather_process.py
@@ -1,11 +1,16 @@
 """This script is badly written on purpose to demonstrate refactoring."""
 import csv, math
 
+PATH_WEATHER_DATA = './../../../data/weather_data.csv'
+TEMP_THRESHOLD_C = 25.0
+FACTOR_C_TO_F = 1.8
+OFFSET_C_TO_F = 32.0
+
 def f(a):
     t=[]
     for i in a:
-        if float(i[1])>25:
-            t.append(float(i[1])*1.8+32)
+        if float(i[1])> TEMP_THRESHOLD_C:
+            t.append(float(i[1]) * FACTOR_C_TO_F + OFFSET_C_TO_F)
         else:
             t.append(float(i[1]))
     return t
@@ -17,7 +22,7 @@ def g(a):
     return s
 
 
-r=open('./../../../data/weather_data.csv')
+r=open(PATH_WEATHER_DATA)
 d=list(csv.reader(r))
 r.close()
 d=d[1:]
@@ -31,5 +36,5 @@ print('avg',z/(len(y) if len(y) else 1))
 ws=0
 for i in d:
     u=float(i[3]); v=float(i[4])
-    ws+=math.sqrt(u*u+v*v)
+    ws+=math.sqrt(u * u + v * v)
 print('wind',ws/len(d))
